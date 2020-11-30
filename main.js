b3da0b52edcf541c01d8b41a6f91e1a62134d09a
yauheni-beiduk <yauheni.beiduk@gmail.com>
2020-11-30 16:32:06 +0300

    fix:add styles and js

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
 const player = document.querySelector('.player');
-const playerButton = document.querySelector('.player-button');
 const video = player.querySelector('.player-video');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress-filled');
+const toggle = player.querySelector('.toggle');
+const skipBtns = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player-input');
+const size = player.querySelector('over-size');
+
+function togglePlay() {
+const playPause = video.paused ? 'play' : 'pause';
+video[playPause]();
+}
+
+function updateBtn() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip)  // +10s or -10s video
+}
+
+function handleRangeUpdate() {
+video[this.name] = this.value;
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.width = `${percent}%`
+}
+
+function scrub(e) {
+const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+video.currentTime = scrubTime
+}
+
+
+
+
+
+
+
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateBtn);
+video.addEventListener('pause', updateBtn);
+video.addEventListener('timeupdate', handleProgress);
+toggle.addEventListener('click', togglePlay);
+skipBtns.forEach(button => button.addEventListener('click',skip));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))  
+progress.addEventListener('click', scrub);
